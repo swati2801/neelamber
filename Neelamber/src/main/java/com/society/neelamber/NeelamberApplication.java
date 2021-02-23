c68279edec7f097af90cd5d31275a32672025783
swati.gupta <swati.gupta@BA-000124984.TI.ads>
2021-02-23 11:02:47 +0530

    First Commit: Bean and initial pom

---
+++ Neelamber/src/main/java/com/society/neelamber/NeelamberApplication.java
@@ -0,0 +1,14 @@
+package com.society.neelamber;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeelamberApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hiijkjlii");
+		SpringApplication.run(NeelamberApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
---
+++ Neelamber/src/test/java/com/society/Neelamber/NeelamberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.society.Neelamber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeelamberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
